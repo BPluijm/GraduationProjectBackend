52bb314dbb0eff14333426f2a16cc014717e704e
BPluijm <75165735+BPluijm@users.noreply.github.com>
2022-04-09 19:07:04 +0200

    created TravelTipsController

---
+++ src/main/java/com/graduation/backend/controller/TravelTipsController.java
@@ -0,0 +1,42 @@
+package com.graduation.backend.controller;
+
+import com.graduation.backend.dto.TravelTipsDto;
+import org.springframework.http.*;
+import org.springframework.validation.*;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class TravelTipsController {
+    private final TravelTipsService service;
+
+    public TravelTipsController(TravelTipsService service) {
+
+        this.service = service;
+    }
+
+    @GetMapping("/trips")
+    public ResponseEntity<Object> getTrips() {
+        List<TravelTipsDto> ld = service.getTravelTips();
+        return new ResponseEntity<>(ld, HttpStatus.OK);
+    }
+
+    @PostMapping("/trips")
+    public ResponseEntity<Object> createTravelTips(@Valid @RequestBody TravelTipsDto ttdto, BindingResult br)  {
+        if (br.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            for(FieldError fe : br.getFieldErrors()) {
+                sb.append(fe.getDefaultMessage());
+                sb.append("\n");
+            }
+            return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
+        } else {
+            service.createTravelTips(ttdto);
+            return new ResponseEntity<>("Tip added!", HttpStatus.CREATED);
+        }
+    }
+}
+}
