3d3102d017d5a2f0e48a799cbf759d2ed92c5432
BPluijm <75165735+BPluijm@users.noreply.github.com>
2022-04-27 16:15:52 +0200

    updated TravelTipsDto

--- src/main/java/com/graduation/backend/dto/TravelTipsDto.java
+++ src/main/java/com/graduation/backend/dto/TravelTipsDto.java
@@ -1,23 +1,37 @@
 package com.graduation.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.graduation.backend.model.FutureTravels;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.*;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TravelTipsDto {
 
-        private final Long id;
+        private Long id;
 
-        @NotBlank
-        private final String name;
+        @NotBlank(message = "Please enter a name.")
+        private String name;
 
-        @NotBlank
-        private final String country;
+        @NotBlank(message = "Please enter a country.")
+        private String country;
 
-        @NotBlank
-        private final String city;
+        @NotBlank(message = "Please enter a city.")
+        private String city;
 
-        @NotBlank
-        private final String description;
+        @NotBlank(message = "Please enter a description.")
+        private String description;
+
+        @JsonIgnore
+        private byte[] folder;
+
+        private FutureTravels future;
 
 }
