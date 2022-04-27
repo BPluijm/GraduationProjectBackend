927d62b941fb3f76dd683950b267c83568d7952c
BPluijm <75165735+BPluijm@users.noreply.github.com>
2022-04-27 16:26:30 +0200

    updated TravelTipsRepository

--- src/main/java/com/graduation/backend/repository/TravelTipsRepository.java
+++ src/main/java/com/graduation/backend/repository/TravelTipsRepository.java
@@ -2,6 +2,8 @@ package com.graduation.backend.repository;
 
 import com.graduation.backend.model.TravelTips;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TravelTipsRepository extends JpaRepository<TravelTips, Long> {
 }
