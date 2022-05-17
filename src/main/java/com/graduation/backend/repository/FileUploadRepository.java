c8a58a996b669c491966b60b17478e71624a2d62
BPluijm <baspluijm@gmail.com>
2022-05-17 14:57:31 +0200

    deleted file upload

--- src/main/java/com/graduation/backend/repository/FileUploadRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.graduation.backend.repository;
-//
-//import com.graduation.backend.model.FileUpload;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public class FileUploadRepository extends CrudRepository<FileUpload, Long> {
-//}
